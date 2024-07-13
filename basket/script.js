b19e24bdaa54be0b4aff9646f36c9e77560d62df
lsoumille <lucas.soumille@etu.unice.fr>
2024-07-13 14:55:36 +0200

    basket menu and carousel

--- basket/script.js
+++ basket/script.js
@@ -18,3 +18,25 @@ document.addEventListener('DOMContentLoaded', function() {
     setInterval(nextSlide, 3000); // Change de slide toutes les 3 secondes
     showSlide(currentIndex);
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Sélectionner tous les liens du menu
+    const menuLinks = document.querySelectorAll('li a[href^="#"]');
+
+    menuLinks.forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault(); // Empêcher le comportement par défaut
+            const targetId = this.getAttribute('href'); // Obtenir l'ID de la section cible
+            const targetSection = document.querySelector(targetId);
+
+            // Calculer la position de la section cible
+            const topOffset = targetSection.offsetTop;
+
+            // Défiler vers la section cible avec une animation
+            window.scrollTo({
+                top: topOffset,
+                behavior: 'smooth'
+            });
+        });
+    });
+});
